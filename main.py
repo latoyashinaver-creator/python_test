bbcb09f762cf08d5d250b1155ade2b2484a4b619
latoya <latoyashinaver@gmail.com>
2025-11-12 09:41:14 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def in_autotests_we_trust(a, b):
+    if a == b:
+        print('Test passed')
+    else:
+        print('Test failed')
+
+in_autotests_we_trust(10, '10')
+
+in_autotests_we_trust(0, False)
